600fbc2316651794fed0c40caaa94fcf8a84e229
pcsishun <pcsishun@Gmail.com>
2021-09-09 17:14:40 +0700

    update image and css

--- src/views/ImgSerach.vue
+++ src/views/ImgSerach.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="imgserach">
-    <h1>This is an find page</h1>
-  </div>
+  <section class="cover">
+    <div class="container">
+      <div class="imgserach" >
+        <h1>This is an Find page</h1>
+      </div>
+    </div>
+ </section>
+
 </template>
+
+
+<script>
+ 
+export default {
+  name: 'imgserach',
+}
+</script>
+
+
+<style >
+.cover{
+  position: relative;
+  overflow: hidden;
+  max-height: 900px;
+  background-image: url('../assets/img/furesign_cover_2.jpg');
+  background-size: cover;
+  background-position: bottom;
+  width: 100%;
+  height: 100vh;
+}
+
+.cover .container {
+  float: left;
+  padding-left: 20%;
+  padding-top: 15%;
+  text-align: left;
+  width: 50%;
+}
+</style>
